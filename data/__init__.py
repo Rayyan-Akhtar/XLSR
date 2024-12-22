2431b0c79e3ce3761a51a4c5b050439c1597be75
Rayyan-Akhtar <akhtar.rayyan@live.com>
2024-12-22 13:03:48 +0530

    updated dataset directory path

--- data/__init__.py
+++ data/__init__.py
@@ -1,9 +1,10 @@
 import os
 from pathlib import Path
+from utils import current_dir
 
 
-dataset_dir = "/teamspace/studios/this_studio/datasets/XLSR"
-output_dir = "/teamspace/studios/this_studio/XLSR/output"
+dataset_dir = os.path.join(current_dir, "dataset")
+output_dir = os.path.join(current_dir, "output")
 os.makedirs(output_dir, exist_ok=True)
 train_data_dir = os.path.join(dataset_dir, "DIV2K_train_HR")
 val_data_dir = os.path.join(dataset_dir, "DIV2K_valid_HR")
